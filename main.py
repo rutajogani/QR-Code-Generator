4513fb926b8252e6e0cbe067a341c7a55ddf7b7d
Ruta Jogani <ruta007jogani@gmail.com>
2026-08-20 23:36:53 +0530

    add server line of code in main.py nad inder.html file change only teg

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+import http.server
+import json
+
 import qrcode as qr
 
-url = input("Enter the URL: ")
-img = qr.make(url)
-img.save("RutaGithubQRCode.png")
-print("---QR Code is Generator---")
+
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(changeUrlToQrCode):
+        
+        url = input("Enter the URL: ")
+        img = qr.make(url)
+        print("---QR Code is Generator---")
+
+        getUrl = urlparse(self.url
+        
+        self.send_response(200)
+
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+
+
+def run():
+    server_address = ("", 8000)
+    httpd = http.server.HTTPServer(server_address, SimpleAPIHandler)
+    print("Python server running on http://localhost:8000 ...")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
---
+++ script.js
@@ -0,0 +1,8 @@
+const createQR = async () => {
+  //get url 
+  // dislpay img
+  const imgId = document.getElementById("python-output");
+  // fatch qr code
+  const qrCode = await fatch("https://github.com/rutajogani");
+   
+ }
